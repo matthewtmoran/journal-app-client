6f67ec8ef4dceb732b43920fe87d1b4097a8fe40
Matthew <matthewtmoran@gmail.com>
2020-07-20 08:46:29 -0500

    remove imports

--- components/EntryPreview.tsx
+++ components/EntryPreview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import IEntry from "../interfaces/IEntry";
 import {
